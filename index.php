99fcc2d3d4ab916f5765410546964cae47da3d32
Alireza Meghdadi <alizz@alirezas-imac.mynet>
2017-12-06 01:07:24 +0000

    Basic files created

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require_once("Includes/db_connection.php"); ?>
+<!DOCTYPE HTML>
+<html lang = "en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial scale=1">
+    <link rel="stylesheet" href="Includes/styles.css">
+
+    <title>Welcome!</title>
+
+</head>
+<body></body>
+<footer></footer>
+
+</html>
+
+<?php
+    // close db connection
+    mysqli_close($connection);
+?>
